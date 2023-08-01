661420c4f128ffdfe969e441fbfc17a9b52b0837
Ali Yavuzyilmaz <aliyavuzyilmaz61@gmail.com>
2023-08-01 19:30:00 +0200

    version 2

--- starterOnly/assets/js/modal.js
+++ starterOnly/assets/js/modal.js
@@ -101,17 +101,76 @@ function validate() {
   
 }
 
-// Get the form
+// Récupérer le formulaire et le corps du modal
 const form = document.forms['reserve'];
+const modalBody = document.querySelector('.modal-body');
 
-// Listen for submit event
+// Sauvegarder le HTML initial du modal pour pouvoir le réinitialiser plus tard
+const initialModalHtml = modalBody.innerHTML;
+
+// Écouter l'événement de soumission du formulaire
 form.addEventListener('submit', function(event) {
-  // Stop the form from submitting
+  // Empêcher la soumission du formulaire
   event.preventDefault();
 
-  // Validate the form
+  // Valider le formulaire
   if (validate()) {
-    // If validation passes, submit the form manually
-    form.submit();
+    // Créer un nouvel élément p
+    const thankYouMessage = document.createElement('p');
+
+    // Lui donner une classe
+    thankYouMessage.className = 'messageRemerciement';
+
+    // Ajouter du texte
+    thankYouMessage.textContent = 'Merci pour votre inscription';
+
+    // Créer un nouveau bouton de fermeture
+    const closeButton = document.createElement('button');
+
+    // Lui donner une classe
+    closeButton.className = 'close-button';
+
+    // Ajouter du texte
+    closeButton.textContent = 'Fermer';
+
+    // Ajouter un écouteur d'événement de clic pour fermer le modal
+    closeButton.addEventListener('click', function() {
+      // Cacher le modal
+      modalbg.style.display = "none";
+
+      // Réinitialiser le HTML du modal à son état initial
+      modalBody.innerHTML = initialModalHtml;
+    });
+
+    // Créer un nouvel élément div
+    const containerDiv = document.createElement('div');
+
+    // Lui donner une classe
+    containerDiv.className = 'message-container';
+
+    // Ajouter le message de remerciement et le bouton de fermeture au div
+    containerDiv.appendChild(thankYouMessage);
+    containerDiv.appendChild(closeButton);
+
+    // Effacer le corps du modal
+    modalBody.innerHTML = '';
+
+    // Ajouter le div au corps du modal
+    modalBody.appendChild(containerDiv);
   }
 });
+
+window.onload = function() {
+  var closeModal = document.getElementsByClassName("close")[0];
+  var modal = document.getElementsByClassName("bground")[0];
+
+  closeModal.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
