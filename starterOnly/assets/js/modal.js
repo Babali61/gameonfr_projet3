29c04832cdffa539774c418e7dc8c82e886a91b4
Ali Yavuzyilmaz <aliyavuzyilmaz61@gmail.com>
2023-10-12 20:28:58 +0200

    reinitialisation formulaire fermeture

--- starterOnly/assets/js/modal.js
+++ starterOnly/assets/js/modal.js
@@ -1,178 +1,176 @@
+// Cette fonction permet de basculer la navigation (probablement une barre de navigation mobile) entre un état réduit et un état étendu.
 function editNav() {
+  // Récupère l'élément de la barre de navigation par son ID.
   var x = document.getElementById("myTopnav");
+
+  // Vérifie si l'élément a la classe "topnav".
   if (x.className === "topnav") {
+    // Si c'est le cas, ajoute la classe "responsive" à l'élément.
     x.className += " responsive";
   } else {
+    // Sinon, réinitialise l'élément à la classe "topnav".
     x.className = "topnav";
   }
 }
 
 // DOM Elements
+// Récupère les éléments nécessaires du DOM.
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// Ajoute un écouteur d'événement à chaque bouton modal pour lancer la modale lors d'un clic.
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Cette fonction affiche la modale.
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// Validation function
+// Cette fonction valide les entrées du formulaire.
 function validate() {
+  // Définit un indicateur de validité initial à vrai.
   let isValid = true;
 
-  const firstName = document.getElementById('first');
-  const firstNameError = document.getElementById('firstName-error');
-  if (firstName.value.trim() === '' || firstName.value.length < 2) {
-    firstNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+  // Validation pour le prénom.
+  const firstName = document.getElementById("first");
+  const firstNameError = document.getElementById("firstName-error");
+  if (firstName.value.trim() === "" || firstName.value.length < 2) {
+    firstNameError.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     isValid = false;
   } else {
-    firstNameError.textContent = '';
+    firstNameError.textContent = "";
   }
 
-  const lastName = document.getElementById('last');
-  const lastNameError = document.getElementById('name-error');
-  if (lastName.value.trim() === '') {
+  // Validation pour le nom.
+  const lastName = document.getElementById("last");
+  const lastNameError = document.getElementById("name-error");
+  if (lastName.value.trim() === "") {
     lastNameError.textContent = "Le champ du nom ne peut pas être vide.";
     isValid = false;
   } else {
-    lastNameError.textContent = '';
+    lastNameError.textContent = "";
   }
 
-  const email = document.getElementById('email');
-  const emailError = document.getElementById('email-error');
-  if (email.value.trim() === '') {
+  // Validation pour l'email.
+  const email = document.getElementById("email");
+  const emailError = document.getElementById("email-error");
+  if (email.value.trim() === "") {
     emailError.textContent = "Le champ de l'e-mail ne peut pas être vide.";
     isValid = false;
   } else {
-    emailError.textContent = '';
+    emailError.textContent = "";
   }
 
-  const birthdate = document.getElementById('birthdate');
-  const birthdateError = document.getElementById('birthdate-error');
-  if (birthdate.value.trim() === '') {
-    birthdateError.textContent = "Le champ de la date de naissance ne peut pas être vide.";
+  // Validation pour la date de naissance.
+  const birthdate = document.getElementById("birthdate");
+  const birthdateError = document.getElementById("birthdate-error");
+  if (birthdate.value.trim() === "") {
+    birthdateError.textContent =
+      "Le champ de la date de naissance ne peut pas être vide.";
     isValid = false;
   } else {
-    birthdateError.textContent = '';
+    birthdateError.textContent = "";
   }
 
-  const quantity = document.getElementById('quantity');
-  const quantityError = document.getElementById('quantity-error');
-  if (quantity.value.trim() === '') {
-    quantityError.textContent = "Le champ de la quantité ne peut pas être vide.";
+  // Validation pour la quantité.
+  const quantity = document.getElementById("quantity");
+  const quantityError = document.getElementById("quantity-error");
+  if (quantity.value.trim() === "") {
+    quantityError.textContent =
+      "Le champ de la quantité ne peut pas être vide.";
     isValid = false;
   } else {
-    quantityError.textContent = '';
+    quantityError.textContent = "";
   }
 
-  // Validate location
-  const locationInputs = Array.from(document.querySelectorAll('input[name="location"]'));
-
-
-  const locationError = document.getElementById('location-error');
+  // Validation pour la localisation.
+  const locationInputs = Array.from(
+    document.querySelectorAll('input[name="location"]')
+  );
+  const locationError = document.getElementById("location-error");
   let isLocationSelected = false;
-
   locationInputs.forEach((input) => {
     if (input.checked) {
       isLocationSelected = true;
     }
   });
-
   if (!isLocationSelected) {
-    locationError.textContent = 'Veuillez choisir une localisation.';
+    locationError.textContent = "Veuillez choisir une localisation.";
     isValid = false;
   } else {
-    locationError.textContent = '';
+    locationError.textContent = "";
   }
 
-  const conditions = document.getElementById('checkbox1');
-  const conditionsError = document.getElementById('conditions-error');
+  // Validation pour les conditions d'utilisation.
+  const conditions = document.getElementById("checkbox1");
+  const conditionsError = document.getElementById("conditions-error");
   if (!conditions.checked) {
-    conditionsError.textContent = "Vous devez accepter les conditions d'utilisation.";
+    conditionsError.textContent =
+      "Vous devez accepter les conditions d'utilisation.";
     isValid = false;
   } else {
-    conditionsError.textContent = '';
+    conditionsError.textContent = "";
   }
 
+  // Retourne l'indicateur de validité.
   return isValid;
-
-  
 }
 
-// Récupérer le formulaire et le corps du modal
-const form = document.forms['reserve'];
-const modalBody = document.querySelector('.modal-body');
+// Récupère le formulaire et le corps du modal.
+const form = document.forms["reserve"];
+const modalBody = document.querySelector(".modal-body");
 
-// Sauvegarder le HTML initial du modal pour pouvoir le réinitialiser plus tard
+// Sauvegarde le HTML initial du modal pour pouvoir le réinitialiser plus tard.
 const initialModalHtml = modalBody.innerHTML;
 
-// Écouter l'événement de soumission du formulaire
-form.addEventListener('submit', function(event) {
-  // Empêcher la soumission du formulaire
+// Écoute l'événement de soumission du formulaire.
+form.addEventListener("submit", function (event) {
+  // Empêche la soumission par défaut du formulaire.
   event.preventDefault();
 
-  // Valider le formulaire
+  // Valide le formulaire.
   if (validate()) {
-    // Créer un nouvel élément p
-    const thankYouMessage = document.createElement('p');
+    // Crée et configure le message de remerciement.
+    const thankYouMessage = document.createElement("p");
+    thankYouMessage.className = "messageRemerciement";
+    thankYouMessage.textContent = "Merci pour votre inscription";
 
-    // Lui donner une classe
-    thankYouMessage.className = 'messageRemerciement';
-
-    // Ajouter du texte
-    thankYouMessage.textContent = 'Merci pour votre inscription';
-
-    // Créer un nouveau bouton de fermeture
-    const closeButton = document.createElement('button');
-
-    // Lui donner une classe
-    closeButton.className = 'close-button';
-
-    // Ajouter du texte
-    closeButton.textContent = 'Fermer';
-
-    // Ajouter un écouteur d'événement de clic pour fermer le modal
-    closeButton.addEventListener('click', function() {
-      // Cacher le modal
+    // Crée et configure le bouton de fermeture.
+    const closeButton = document.createElement("button");
+    closeButton.className = "close-button";
+    closeButton.textContent = "Fermer";
+    closeButton.addEventListener("click", function () {
       modalbg.style.display = "none";
-
-      // Réinitialiser le HTML du modal à son état initial
       modalBody.innerHTML = initialModalHtml;
     });
 
-    // Créer un nouvel élément div
-    const containerDiv = document.createElement('div');
-
-    // Lui donner une classe
-    containerDiv.className = 'message-container';
-
-    // Ajouter le message de remerciement et le bouton de fermeture au div
+    // Crée un conteneur pour le message et le bouton.
+    const containerDiv = document.createElement("div");
+    containerDiv.className = "message-container";
     containerDiv.appendChild(thankYouMessage);
     containerDiv.appendChild(closeButton);
 
-    // Effacer le corps du modal
-    modalBody.innerHTML = '';
-
-    // Ajouter le div au corps du modal
+    // Efface le contenu du modal et y ajoute le conteneur.
+    modalBody.innerHTML = "";
     modalBody.appendChild(containerDiv);
   }
 });
 
-window.onload = function() {
+// Fermeture de la modale lors du clic sur la croix ou en dehors de la modale.
+window.onload = function () {
   var closeModal = document.getElementsByClassName("close")[0];
   var modal = document.getElementsByClassName("bground")[0];
 
-  closeModal.onclick = function() {
+  closeModal.onclick = function () {
     modal.style.display = "none";
-  }
+  };
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      modalBody.innerHTML = initialModalHtml;
     }
-  }
-}
+  };
+};
